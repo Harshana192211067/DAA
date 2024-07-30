a8e091d66eca9fdc011a31bace35bb6abfa99698
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2024-07-30 20:42:32 +0530

    Add files via upload

--- 23.Binomial Coefficient.cpp
+++ 23.Binomial Coefficient.cpp
@@ -1,31 +1,25 @@
 #include<stdio.h>
+
 int main()
 {
-int n, a, bi_nom,limit;
-printf("Enter the limit:");
-scanf("%d",&limit);
-for (n=0; n<=limit; ++n)
-printf("%d   ", n);
-n=0;
-printf("\n");
-do
-{
-a = 0, bi_nom = 1; 
-printf("\n");
-printf("%d", n);
-while(a<=n)
-{
-if(n==0 || a==0)
-printf("%4d", bi_nom);
-else
-{
-bi_nom=bi_nom* (n-a+1)/a;
-printf("%4d", bi_nom);
+    int n, k;
+    printf("Enter n and k : ");
+    scanf("%d%d", &n, &k);
+    
+    int C[n+1][k+1];
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= (i < k ? i : k); j++) {
+            if (j == 0 || j == i) {
+                C[i][j] = 1;
+            } else {
+                C[i][j] = C[i-1][j-1] + C[i-1][j];
+            }
+        }
+    }
+    
+    printf("Binomial coefficient: %d\n", C[n][k]);
+
+    return 0;
 }
-a=a+1;
-}
-printf("\n");
-n=n+1;
-}
-while(n<=limit);
-}
+
